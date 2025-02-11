70731a418d55e75ac273a24ef8400d35aab59315
Professor <Professor@SN-388330.edu.fiesc.com.br>
2025-02-11 20:57:59 -0300

    create entity orderItem

--- src/main/java/com/senai/ecommerce/entities/OrderItem.java
+++ src/main/java/com/senai/ecommerce/entities/OrderItem.java
@@ -1,5 +1,18 @@
 package com.senai.ecommerce.entities;
 
-public class OrderItem {
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private int quantity;
+    private double price;
 }
--- src/main/java/com/senai/ecommerce/entities/OrderItemPK.java
+++ src/main/java/com/senai/ecommerce/entities/OrderItemPK.java
@@ -1,7 +1,19 @@
 package com.senai.ecommerce.entities;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Data
 @Embeddable
 public class OrderItemPK {
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
 }
