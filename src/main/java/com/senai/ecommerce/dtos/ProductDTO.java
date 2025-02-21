e4cc00583622d3266b5913035e6319f4fa6ced68
= <ezequiasmoreira@hotmail.com>
2025-02-21 19:35:15 -0300

    add ProductDTO

---
+++ src/main/java/com/senai/ecommerce/dtos/ProductDTO.java
@@ -0,0 +1,27 @@
+package com.senai.ecommerce.dtos;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductDTO {
+    private Long id;
+    @Size(min = 2, max = 100, message = "O Campo nome precisa ter entre 2 e 100 caracteres")
+    @NotBlank(message = "O campo nome não pode ser vazio")
+    private String name;
+    @Size(min = 10, message = "O campo descrição precisa ter entre 2 e 500 caracteres")
+    @NotBlank(message = "O campo descrição não pode ser vazio")
+    private String description;
+    @NotNull(message = "O campo preço não pode ser vazio")
+    @Positive(message = "O campo preço precisa ser positivo")
+    private double price;
+    private String imgUrl;
+    @NotEmpty(message = "O campo categorias não pode ser vazio")
+    private List<CategoryDTO> categories;
+}
